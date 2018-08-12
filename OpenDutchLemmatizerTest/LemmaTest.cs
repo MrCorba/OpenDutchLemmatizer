1aff6bcc349845b2c5a8ce94cabd63ca8df26173
MrCorba <jespercorba91@gmail.com>
2018-08-12 14:21:05 +0200

    Add test to count number of correct lemmatizations

--- OpenDutchLemmatizerTest/LemmaTest.cs
+++ OpenDutchLemmatizerTest/LemmaTest.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenDutchLemmatizer;
+using System;
+using System.IO;
 
 namespace OpenDutchLemmatizerTest
 {
@@ -21,6 +23,32 @@ namespace OpenDutchLemmatizerTest
             var lemma = TestContext.DataRow.ItemArray[0].ToString();
             var form = TestContext.DataRow.ItemArray[1].ToString();
             Assert.AreEqual(lemma, Lemmatizer.GetLemma(form));
+        }
+
+        [TestMethod]
+        public void TestCountLemmatizer()
+        {
+            var dir = Environment.CurrentDirectory;
+            var testFile = Path.Combine(dir, "TestData", "dutch_lemmas.csv");
+            string line;
+
+            var goodCount = 0;
+            var totalCount = 0;
+
+            using (var reader = new StreamReader(testFile))
+            {
+                while ((line = reader.ReadLine()) != null)
+                {
+                    var words = line.Split(',');
+
+                    if (words[0] == Lemmatizer.GetLemma(words[1]))
+                        goodCount++;
+
+                    totalCount++;
+                }
+            }
+
+            Assert.AreEqual(totalCount, goodCount);
         }
     }
 }
